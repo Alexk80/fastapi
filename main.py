82169516d88aab1fde2fcb5cc9ccd23d083a5bae
Alexk80 <kazanskiy80@gmail.com>
2025-12-18 23:35:57 +0500

    Подготовил рабочий вариант

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+from datetime import date
 from fastapi import FastAPI, Request, HTTPException
 import pickle
 import pandas as pd
-from pydantic import BaseModel
+import numpy as np
+from pydantic import BaseModel, Field
 app = FastAPI()
 
 # Загрузка модели из файла pickle
-with open('model.pkl', 'rb') as f:
+with open('best_model.pkl', 'rb') as f:
     model = pickle.load(f)
+    
+with open('samples.csv', 'r') as file:
+    data = pd.read_csv(file)
+    
 
+with open('scaler_42.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+    
 # Счетчик запросов
 request_count = 0
 
 # Модель для валидации входных данных
 class PredictionInput(BaseModel):
-    Pclass: int
-    Age: float
-    Fare: float
+    number: int
 
 @app.get("/stats")
 def stats():
@@ -25,23 +32,28 @@ def stats():
 def health():
     return {"status": "OK"}
 
+@app.get("/samples")
+def sample():
+    return {"request_count": [data.iloc[0]]}
+
 @app.post("/predict_model")
 def predict_model(input_data: PredictionInput):
     global request_count
     request_count += 1
 
     # Создание DataFrame из данных
-    new_data = pd.DataFrame({
+    
+    '''new_data = pd.DataFrame({
         'Pclass': [input_data.Pclass],
         'Age': [input_data.Age],
         'Fare': [input_data.Fare]
-    })
+    })'''
 
     # Предсказание
-    predictions = model.predict(new_data)
+    predictions = model.predict([data.iloc[input_data.number]])
 
     # Преобразование результата в человеко-читаемый формат
-    result = "Survived" if predictions[0] == 1 else "Not Survived"
+    result = "Пользователь совершил действие" if predictions[0] == 1 else "Пользователь не совершал действий"
 
     return {"prediction": result}
 
